e750da2735c1dd5f98be1349af8d846b63e11719
James Stewart <jmarshallstewart@gmail.com>
2021-05-22 00:36:17 -0500

    Adds player walk and idle anim transitions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 6.0f;
     public float turnSmoothTime = 0.1f;
     public float minDirectionChangeThreshold = 0.1f;
+    public Animator animator;
 
     CharacterController characterController;
+    
     float turnSmoothVelocity;
 
     void Start()
@@ -23,8 +25,11 @@ public class PlayerController : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
         Vector3 desiredDirection = new Vector3(h, 0.0f, v).normalized;
 
+        //animator.SetInteger("state", desiredDirection.magnitude > 0 ? 1 : 0);
+
         if (desiredDirection.magnitude >= minDirectionChangeThreshold)
         {
+            animator.SetInteger("state", 1);
             float targetAngle = Mathf.Atan2(desiredDirection.x, desiredDirection.z) * Mathf.Rad2Deg + sceneCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
@@ -32,6 +37,10 @@ public class PlayerController : MonoBehaviour
             
             Vector3 moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
             characterController.SimpleMove(moveDirection * walkSpeed);
+        }
+        else
+        {
+            animator.SetInteger("state", 0);
         }
     }
 }
