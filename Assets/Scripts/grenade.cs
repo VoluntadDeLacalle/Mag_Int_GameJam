f1e91a05ad72d14f93629036ac16ead338a8ee5f
Luis Hernandez <luis777_hdz_11@hotmail.com>
2021-05-21 19:27:11 -0500

    WIP explosions
    -Grenade and landmine models
    -low poly explosion
    -code for explosion

---
+++ Assets/Scripts/grenade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenade : MonoBehaviour
+{
+    public float timer = 2;
+    private float countdown;
+    private bool hasExploded = false;
+
+    [SerializeField] GameObject explodeParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0 && !hasExploded)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Instantiate(explodeParticle, transform.position, transform.rotation);
+        hasExploded = true;
+    }
+}
