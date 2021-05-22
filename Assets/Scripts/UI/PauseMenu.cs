ee4ed57cb27c409ac0d7dce37a7e5bc58386f607
Rebekah <rdleamons@gmail.com>
2021-05-21 23:40:15 -0500

    Adds start and pause menus

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Button menuButton;
+    public Button resumeButton;
+    public Button quitButton;
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game...");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button startButton;
+    public Button optionsButton;
+    public Button quitButton;
+    public Button backButton;
+
+    public GameObject optionsUI;
+
+    public void Start()
+    {
+        optionsUI.SetActive(false);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("PlayerGym");
+    }
+
+    public void Options()
+    {
+        
+        if (optionsUI.activeSelf == false)
+        {
+            optionsUI.SetActive(true);
+        }
+        else
+        {
+            optionsUI.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
