69aa2561e689f7c72480f7eac33b83fbcffb94d1
William Gavin Lacalle <digitalbros7@gmail.com>
2021-10-19 18:11:48 -0500

    Updates billboard

--- Assets/Scripts/UI/UI Utilities/Billboard.cs
+++ Assets/Scripts/UI/UI Utilities/Billboard.cs
@@ -7,5 +7,6 @@ public class Billboard : MonoBehaviour
     void Update()
     {
         transform.LookAt(Player.Instance.vThirdPersonCamera.target.position, Vector3.up);
+        transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
     }
 }
